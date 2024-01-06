7c237f2163d60862bce1ffdb642a3c17d2e6545c
patilarya76 <patilaryaanil07@gmail.com>
2024-01-06 15:30:52 +0530

    Done with live session

--- src/App.jsx
+++ src/App.jsx
@@ -20,11 +20,34 @@ const imageData = ()=>{
       img:elephant
     }
   ]
-  return data;
+  return data;  //data is array of objects
 }
-
+let data=imageData()
 function App() {
-  // code here
+   //find using let how can styling be applied
+  //  let galleryStyle={
+  //  display:"grid",
+  //  }
+   let styleMydiv={
+    display:"grid",
+    gridTemplateColumns:"repeat(2,1fr)",
+    marginLeft:"200px",
+    
+   }
+
+
+   return(
+    <>
+    <h1 style={{textAlign:'center'}}>Kalvium Gallery</h1>
+    <div style={styleMydiv}>
+      <img  src={data[0].img} alt="" width={"500px"}/>
+      <img src={data[1].img} alt="" width={"500px"}/>
+      <img style={{marginTop:"50px"}} src={data[2].img} alt="" width={"500px"}/>
+      <img style={{marginTop:"50px"}} src={data[3].img} alt="" width={"500px"}/>
+    </div>
+    
+    </>
+   )
 }
 
 export default App;
--- src/AppClass.jsx
+++ src/AppClass.jsx
@@ -26,5 +26,31 @@ export default class AppClass extends Component{
     return data;
   }
 
-  // code here
+  render(){
+      this.data=this.imageData()
+      console.log(this.data)
+
+      let galleryStyle={
+        textAlign:"centre"
+      }
+
+      let styleMydiv={
+        display:"grid",
+        gridTemplateColumns:"repeat(2,1fr)",
+        marginLeft:"200px",
+        
+       }
+    return(
+      <>
+      <h1 style={{textAlign:'center'}}>Kalvium Gallery</h1>
+      <div style={styleMydiv}>
+        <img src={this.data[0].img} alt="" width={"500px"}/>
+        <img src={this.data[1].img} alt="" width={"500px"}/>
+        <img style={{marginTop:"50px"}} src={this.data[2].img} alt="" width={"500px"}/>
+        <img style={{marginTop:"50px"}} src={this.data[3].img} alt="" width={"500px"}/>
+      </div>
+      
+      </>
+    )
+  }
 }
